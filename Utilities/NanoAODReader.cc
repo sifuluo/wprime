f4530ae0d07f7aa46ced9d9f4f292c45a26b6959
Sifu Luo <sifuluo@tamu.edu>
2023-02-01 09:48:04 +0800

    Changed bTagEff[] order

--- Utilities/NanoAODReader.cc
+++ Utilities/NanoAODReader.cc
@@ -220,7 +220,7 @@ public:
       tmp.bJetSFweights = {{1.,1.,1.}, {1.,1.,1.}, {1.,1.,1.}};
       if (IsMC) {
         //FIXME: Need b-tagging efficiency per sample at some point, see https://twiki.cern.ch/twiki/bin/viewauth/CMS/BTagSFMethods#b_tagging_efficiency_in_MC_sampl
-        float bTagEff[3] = {.9, .7, .5};
+        float bTagEff[3] = {.5, .7, .9};
         if(tmp.bTagPasses[0]){
           tmp.bJetSFweights[0][0] = evts->Jet_bTagScaleFactorLoose[i];
           tmp.bJetSFweights[1][0] = evts->Jet_bTagScaleFactorLooseUp[i];
